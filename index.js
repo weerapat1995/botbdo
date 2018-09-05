76a72d685f5504c6bb99075c4a6a950430494cef
วีรภัทร แก่นจันทร์ <weerapat_p_1995@hotmail.com>
2018-09-05 18:26:56 +0700

    Update index.js

--- index.js
+++ index.js
@@ -11,7 +11,8 @@ client.on("message" , message =>{
     if(!message.content.startsWith("$")) return;
     var xxx = message.content.slice(1).split(" ")[1];
     // var nameitem = new toString(xxx);
-    var alert = setInterval(Store,840000);
+    // var alert = setInterval(Store,840000);
+    var alert = setInterval(Store,2000);
     function Store(){
     message.channel.send(`${message.author}, ภายใน 1 นาที `+ xxx +` จะลง`)
         clearInterval(alert);
