38657a8a77f8654a4a8d9ce46260e2124547a0f3
วีรภัทร แก่นจันทร์ <weerapat_p_1995@hotmail.com>
2018-09-05 17:51:52 +0700

    Create index.js

---
+++ index.js
@@ -0,0 +1,21 @@
+const Discord = require('discord.js')
+const client = new Discord.Client()
+
+client.login('NDc5OTY3MjEwNjY0NjI0MTMw.DlhP0g.uT-UAK-nkkUFg8Kl-qCVrnGFOk0')
+
+client.on("ready",()=>{
+    console.log('HELLO!')
+})
+
+client.on("message" , message =>{
+    if(!message.content.startsWith("$")) return;
+    var xxx = message.content.slice(1).split(" ")[1];
+    // var nameitem = new toString(xxx);
+    var alert = setInterval(Store,840000);
+    function Store(){
+    message.channel.send(`${message.author}, ภายใน 1 นาที `+ xxx +` จะลง`)
+        clearInterval(alert);
+
+        
+    }
+})
